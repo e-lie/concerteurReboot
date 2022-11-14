b8ef8a935032ecc16615093a66415cf9593b91bb
Elie Gavoty <eliegavoty@free.fr>
2022-11-14 18:14:54 +0100

    add login mechanism

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -3,17 +3,42 @@ import os
 from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
-
-from .main import main as main_blueprint
-
-
-#from .models import User
+from flask_login import LoginManager 
 
 
-app = Flask(__name__)
-app.config.from_object("project.config.Config")
-db = SQLAlchemy(app)
-app.register_blueprint(main_blueprint)
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object("project.config.Config")
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+
+app = create_app()
 
 
 
--- services/web/project/models.py
+++ services/web/project/models.py
@@ -1,12 +1,16 @@
 from . import db
+from flask_login import UserMixin
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True, nullable=False)
     active = db.Column(db.Boolean(), default=True, nullable=False)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
 
-    def __init__(self, email):
+    def __init__(self, email, name, password):
         self.email = email
+        self.name = name
+        self.password = password
